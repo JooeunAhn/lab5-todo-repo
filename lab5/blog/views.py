0c1e3a6400d1d60724896c8d5e5bd19f17239449
JooeunAhn <d17123391@manager.c.d17123391.internal>
2017-10-23 04:27:40 +0000

    done

---
+++ lab5/blog/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import TodoForm
+from .models import Todo
+
+# Create your views here.
+
+def index(request):
+    return render(request, "blog/index.html", {})
+
+def todo_list(request):
+    todo_list = Todo.objects.all()
+    return render(request, "blog/list.html", {"todo_list": todo_list})
+
+def todo_add(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = TodoForm()
+    return render(request, "blog/forms.html", {"form": form})
+
+def todo_update(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'blog/forms.html', {"form": form})
+
+
+def todo_delete(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.delete()
+    return redirect('list')
+
